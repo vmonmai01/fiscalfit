a4930b14e057e766e5c486ce49b36a93a7523c33
vmonmai01 <victormm95314@gmail.com>
2024-04-16 11:51:24 +0200

    Commit ChartBtwIncExp bien

--- resources/js/chartBtwIncExp.js
+++ resources/js/chartBtwIncExp.js
@@ -2,74 +2,78 @@ document.addEventListener("DOMContentLoaded", function() {
     var myChartBar = document.getElementById("barChartBtwIncExp");
     var barChart = null; // Variable para mantener una referencia al gráfico renderizado
 
-    const options = {
-        series: [
-            {
-                name: "Income",
-                color: "#31C48D",
-                data: [],
+    // Función para hacer la llamada a la API y actualizar/renderizar el gráfico
+    function renderChart(numMonths, chartHeight) {
+        var options = {
+            series: [
+                {
+                    name: "Income",
+                    color: "#31C48D",
+                    data: [],
+                },
+                {
+                    name: "Expense",
+                    data: [],
+                    color: "#F05252",
+                }
+            ],
+            chart: {
+                type: "bar",
+                height: chartHeight || 400, // Establecer la altura predeterminada como 400 si no se proporciona un valor específico
+                toolbar: {
+                    show: false
+                }
             },
-            {
-                name: "Expense",
-                data: [],
-                color: "#F05252",
-            }
-        ],
-        chart: {
-            type: "bar",
-            height: 400,
-        },
-        plotOptions: {
-            bar: {
-                horizontal: true,
-                columnWidth: "100%",
-                borderRadius: 6,
-                dataLabels: {
-                    position: "top",
+            plotOptions: {
+                bar: {
+                    horizontal: true,
+                    columnWidth: "100%",
+                    borderRadius: 6,
+                    dataLabels: {
+                        position: "top",
+                    },
                 },
             },
-        },
-        legend: {
-            show: true,
-            position: "bottom",
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        xaxis: {
-            labels: {
+            legend: {
                 show: true,
+                position: "bottom",
             },
-            categories: [],
-        },
-        yaxis: {
-            labels: {
+            dataLabels: {
+                enabled: false,
+            },
+            xaxis: {
+                labels: {
+                    show: true,
+                },
+                categories: [],
+            },
+            yaxis: {
+                labels: {
+                    show: true,
+                }
+            },
+            tooltip: {
+                shared: true,
+                intersect: false,
+                formatter: function (value) {
+                    return "$" + value
+                }
+            },
+            grid: {
                 show: true,
-            }
-        },
-        tooltip: {
-            shared: true,
-            intersect: false,
-            formatter: function (value) {
-                return "$" + value
-            }
-        },
-        grid: {
-            show: true,
-            strokeDashArray: 4,
-            padding: {
-                left: 2,
-                right: 2,
-                top: -20
+                strokeDashArray: 4,
+                padding: {
+                    left: 2,
+                    right: 2,
+                    top: -20
+                },
             },
-        },
-        fill: {
-            opacity: 1,
-        }
-    }
+            fill: {
+                opacity: 1,
+            },
+            
+        };
 
-    // Función para hacer la llamada a la API y actualizar/renderizar el gráfico
-    function renderChart(numMonths) {
         $.ajax({
             url: `http://127.0.0.1:8000/api/finance/1/${numMonths}/expenses-incomes`,
             method: "GET",
@@ -103,21 +107,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Asociar eventos de clic a los botones usando addEventListener
     document.getElementById("btn3").addEventListener("click", function() {
-        renderChart(3);
+        renderChart(3, 300); // Llama a renderChart con 3 meses y altura de 300px
     });
 
     document.getElementById("btn6").addEventListener("click", function() {
-        renderChart(6);
+        renderChart(6, 400); // Llama a renderChart con 6 meses y altura de 400px
     });
 
     document.getElementById("btn9").addEventListener("click", function() {
-        renderChart(9);
+        renderChart(9, 500); // Llama a renderChart con 9 meses y altura de 500px
     });
 
     document.getElementById("btn12").addEventListener("click", function() {
-        renderChart(12);
+        renderChart(12, 600); // Llama a renderChart con 12 meses y altura de 600px
     });
 
-    // Llamada inicial al cargar la página con 3 meses
+    // Llamada inicial al cargar la página con 3 meses y altura predeterminada de 400px
     renderChart(3);
 });
