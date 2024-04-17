8f9bbc05a5cd00edec85ef5823519af83ee1329c
vmonmai01 <victormm95314@gmail.com>
2024-04-17 14:10:45 +0200

    Commit chartBtwIncExp 20240417

--- resources/js/chartBtwIncExp.js
+++ resources/js/chartBtwIncExp.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var myChartBar = document.getElementById("barChartBtwIncExp");
     var barChart = null; // Variable para mantener una referencia al gráfico renderizado
 
@@ -71,14 +71,14 @@ document.addEventListener("DOMContentLoaded", function() {
             fill: {
                 opacity: 1,
             },
-            
+
         };
 
         $.ajax({
             url: `http://127.0.0.1:8000/api/finance/1/${numMonths}/expenses-incomes`,
             method: "GET",
             dataType: "json",
-            success: function(data) {
+            success: function (data) {
                 // Actualizar los datos del gráfico con los datos recibidos de la API
                 options.series[0].data = data.map(item => item.income);
                 options.series[1].data = data.map(item => item.expenses);
@@ -98,27 +98,61 @@ document.addEventListener("DOMContentLoaded", function() {
                     barChart = new ApexCharts(myChartBar, options);
                     barChart.render();
                 }
+
+                 // Llamar a la función para actualizar los elementos HTML con los resultados calculados
+                 updateHTML(data);
+
+                
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.error('Error al obtener los datos de la API:', error);
             }
         });
     }
 
+
+    function updateHTML(data) {
+        // Calcular los totales de ingresos y gastos
+        let totalIncome = 0;
+        let totalExpenses = 0;
+
+        data.forEach(item => {
+            // Convertir los valores de ingresos y gastos a números
+            const income = parseFloat(item.income);
+            const expenses = parseFloat(item.expenses);
+
+            // Sumar al total de ingresos y gastos
+            totalIncome += income;
+            totalExpenses += expenses;
+        });
+
+        // Calcular la diferencia entre ingresos y gastos
+        const difference = totalIncome - totalExpenses;
+
+        // Mostrar los resultados
+        console.log(`Ingresos totales: ${totalIncome.toFixed(2)}`);
+        console.log(`Gastos totales: ${totalExpenses.toFixed(2)}`);
+        console.log(`Diferencia: ${difference.toFixed(2)}`);
+
+        // Actualizar los elementos HTML con los resultados calculados
+        document.getElementById("totalIncome").textContent = "$" + totalIncome.toFixed(2);
+        document.getElementById("totalExpenses").textContent = "$" + totalExpenses.toFixed(2);
+        document.getElementById("Diferencia").textContent = "$" + difference.toFixed(2);
+    }
     // Asociar eventos de clic a los botones usando addEventListener
-    document.getElementById("btn3").addEventListener("click", function() {
+    document.getElementById("btn3").addEventListener("click", function () {
         renderChart(3, 300); // Llama a renderChart con 3 meses y altura de 300px
     });
 
-    document.getElementById("btn6").addEventListener("click", function() {
+    document.getElementById("btn6").addEventListener("click", function () {
         renderChart(6, 400); // Llama a renderChart con 6 meses y altura de 400px
     });
 
-    document.getElementById("btn9").addEventListener("click", function() {
+    document.getElementById("btn9").addEventListener("click", function () {
         renderChart(9, 500); // Llama a renderChart con 9 meses y altura de 500px
     });
 
-    document.getElementById("btn12").addEventListener("click", function() {
+    document.getElementById("btn12").addEventListener("click", function () {
         renderChart(12, 600); // Llama a renderChart con 12 meses y altura de 600px
     });
 
--- resources/views/components/chart-bar-diference-btw-inc-exp.blade.php
+++ resources/views/components/chart-bar-diference-btw-inc-exp.blade.php
@@ -1,11 +1,29 @@
-
-<div>
-    <div id="barChartBtwIncExp" style="width: 400px; height: 300px;"></div>
-
+<div class="max-w-md w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+    <div class="flex justify-between border-gray-200 border-b dark:border-gray-700 pb-3">
+        <dl>
+            <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Profit</dt>
+            <dd id="Diferencia" class="leading-none text-3xl font-bold text-gray-900 dark:text-white">$5,405</dd>
+        </dl>
+    </div>
     <div>
-        <button id="btn3" class="btn btn-primary">3 Meses</button>
-        <button id="btn6" class="btn btn-primary">6 Meses</button>
-        <button id="btn9" class="btn btn-primary">9 Meses</button>
-        <button id="btn12" class="btn btn-primary">12 Meses</button>
+        <div class="grid grid-cols-2 py-3 ">
+            <dl>
+                <dt  class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1 ml-3">Income</dt>
+                <dd id="totalIncome" class="leading-none text-xl font-bold text-green-500 dark:text-green-400">$23,635</dd>
+            </dl>
+            <dl>
+                <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Expense</dt>
+                <dd id="totalExpenses" class="leading-none text-xl font-bold text-red-600 dark:text-red-500">-$18,230</dd>
+            </dl>
+        </div>
+
+        <div id="barChartBtwIncExp" style="width: 400px; height: 300px;"></div>
+
+        <div>
+            <button id="btn3" class="btn btn-primary">3 Meses</button>
+            <button id="btn6" class="btn btn-primary">6 Meses</button>
+            <button id="btn9" class="btn btn-primary">9 Meses</button>
+            <button id="btn12" class="btn btn-primary">12 Meses</button>
+        </div>
     </div>
 </div>
