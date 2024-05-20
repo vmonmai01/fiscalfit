3ddd0bb09883b337329ea8e3147c1637f44ed61f
vmonmai01 <victormm95314@gmail.com>
2024-05-20 13:04:50 +0200

    Commit notifications view blade retocado

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,16 +1,21 @@
 <x-app-layout>
-    <h1> Hola hola </h1>
-    {{auth()->user()->name}}
-    Gastos:{{auth()->user()->expenses()->sum('amount')}}
-    <br>
-    Ingresos:{{auth()->user()->incomes()->sum('amount')}}
-    {{auth()->user()->name}}
 
     @if ($notifications->isNotEmpty())
-    <p>Entra en notificaciones</p>
-    @foreach ($notifications as $notification)
-        <h2>Mensaje: {{ $notification->message }}</h2>
-    @endforeach
+        
+        @foreach ($notifications as $notification)
+            <hr>
+
+            <h3>Descripción del gasto: {{ $notification->expense->description }}</h3>
+
+            <h3>Importe gasto: {{ $notification->expense->amount }}</h3>
+
+            <h3>Fecha del gasto: {{ $notification->expense->date }}</h3>
+
+            <h3>Fecha notificación: {{ $notification->send_at }}</h3>
+            
+            <hr>
+            
+        @endforeach
     @else
         <p>No tienes notificaciones.</p>
     @endif
