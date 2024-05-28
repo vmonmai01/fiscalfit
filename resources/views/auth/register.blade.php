24924a7729a5bd059464e07acfa2bb355285220c
vmonmai01 <vmonmai01@gmail.com>
2024-05-28 19:46:28 +0200

    Commit login register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,78 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        <!-- lastname	birthdate	currency_preference	avatar	rol	 -->
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Nombre')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
-                autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+    <div class="flex justify-center items-center w-full sm:max-w-lg ">
+        
+        <div class="flex-1">
+            <div>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <!-- lastname	birthdate	currency_preference	avatar	rol	 -->
+                    <!-- Name -->
+                    <div>
+                        <x-input-label for="name" :value="__('Nombre')" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                            :value="old('name')" required autofocus autocomplete="name" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+                    <!--LastName -->
+                    <div>
+                        <x-input-label for="lastname" :value="__('Apellido/s')" />
+                        <x-text-input id="lastname" class="block mt-1 w-full" type="text" name="lastname"
+                            :value="old('lastname')" required autofocus autocomplete="lastname" />
+                        <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
+                    </div>
+                    <!-- Birthdate -->
+                    <div>
+                        <x-input-label for="birthdate" :value="__('Fecha de Nacimiento')" />
+                        <x-text-input id="birthdate" class="block mt-1 w-full" type="date" name="birthdate"
+                            :value="old('birthdate')" required autofocus autocomplete="birthdate" />
+                        <x-input-error :messages="$errors->get('birthdate')" class="mt-2" />
+                    </div>
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                            :value="old('email')" required autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Contraseña')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                            autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password_confirmation" :value="__('Confirme contraseña')" />
+
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            href="{{ route('login') }}">
+                            {{ __('Already registered?') }}
+                        </a>
+
+                        <x-primary-button class="ms-4">
+                            {{ __('Register') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+            </div>            
         </div>
-        <!--LastName -->
-        <div>
-            <x-input-label for="lastname" :value="__('Apellido/s')" />
-            <x-text-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" :value="old('lastname')"
-                required autofocus autocomplete="lastname" />
-            <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
+        {{-- Imagen --}}
+        <div class="flex-1 h-full ml-2">
+            <img src="{{ asset('storage/fiscalfit/register.png') }}" alt="Side Image"
+                class="w-full h-full object-cover">
         </div>
-        <!-- Birthdate -->
-        <div>
-            <x-input-label for="birthdate" :value="__('Fecha de Nacimiento')" />
-            <x-text-input id="birthdate" class="block mt-1 w-full" type="date" name="birthdate" :value="old('birthdate')"
-                required autofocus autocomplete="birthdate" />
-            <x-input-error :messages="$errors->get('birthdate')" class="mt-2" />
-        </div>
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Contraseña')" />
-
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirme contraseña')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,15 +17,15 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-medio">
             <div>
-                {{-- <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a> --}}
+                <a href="/">
+                    <img class="w-auto h-24" src="{{asset('storage/fiscalfit/logo.png')}}">
+                </a> 
             </div>
-
+            {{-- <div>
+                <img src="{{asset('storage/fiscalfit/logo.png')}}">
+            </div> --}}
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-oscuro shadow-md overflow-hidden sm:rounded-lg">
-                <div>
-                    <img src="{{asset('storage/fiscalfit/logo.png')}}">
-                </div>
+                
                 <div>
                     {{ $slot }}
                 </div>
